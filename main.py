f6705c83c2c6de7ca375c6beb599a84a918f6673
mati <matizieba@gmail.com>
2022-03-09 08:43:13 +0100

    final commit

--- main.py
+++ main.py
@@ -19,12 +19,13 @@ class TicTacToe:
             print(*row)
 
     def get_random_first_player(self):
-        return random.int(0,1)
+        return random.randint(0, 1)
 
     def fix_spot(self, row, col, player):
         self.board[row][col] = player
 
     def checking_if_players_won(self, player):
+        win = None
 #checking row
         for i in range(3):
             win = True
@@ -54,12 +55,14 @@ class TicTacToe:
             return win
 
         win = True
-        for i in range(2-i):
-            if self.board[i][i] != player:
+        for i in range(3):
+            win = True
+            if self.board[i][2-i] != player:
                 win = False
                 break
         if win:
             return win
+        return False
 
     def checking_if_board_full(self):
         is_full = True
@@ -69,16 +72,41 @@ class TicTacToe:
                     is_full = False
         return is_full
 
+    def swap_player(self, player):
+        return "X" if player == "o" else "o"
 
+    def start(self):
+        self.create_board()
 
+        player = "X" if self.get_random_first_player()==1 else "o"
+        while True:
+            print(f"Player {player} turn")
 
+#collectling users input
 
+            self.show_board()
+            row,col = list(map(int, input("Enter row and column number to fix the spot")))
 
+#fixing a spot
 
+            self.fix_spot(row -1, col - 1, player)
 
+#checking if player has won
 
+            if self.checking_if_players_won(player):
+                print(f"Player {player} won!")
+                break
+
+#checking if is draw
+
+            if self.checking_if_board_full():
+                print(f"its a draw!")
+                break
+
+#swaping player
+            player = self.swap_player(player)
 
 
 game = TicTacToe()
-game.create_board()
-game.show_board()
+game.start()
+
